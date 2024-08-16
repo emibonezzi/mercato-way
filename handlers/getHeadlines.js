e669dc58ad32fba958c8b56dd0467bd64c907a82
emibonezzi <emibonezzi@gmail.com>
2024-08-16 14:11:10 -0400

    Add 3 hashtag

--- handlers/getHeadlines.js
+++ handlers/getHeadlines.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const input =
-  "You are receiving an update with a title and source url. Take this style of reporting as an example: `⚪️🇫🇷 Tottenham invest important fee on French winger Wilson Odobert as he was in the list of several clubs in UK and Italy.\n\n£25m fixed fee, £5m add-ons and also 10% sell-on clause for Burnley.\n\nFive year deal, shirt no. 29 and medical also completed in London. 🪄` and transform the title you're receiving in that format. Either extract the source name from the title or from the url string. Add a max of 3 relevant hashtags and make sure you stay in an acceptable length typical of a tweet";
+  "You are receiving an update with a title and source url. Take this style of reporting as an example: `⚪️🇫🇷 Tottenham invest important fee on French winger Wilson Odobert as he was in the list of several clubs in UK and Italy.\n\n£25m fixed fee, £5m add-ons and also 10% sell-on clause for Burnley.\n\nFive year deal, shirt no. 29 and medical also completed in London. 🪄` and transform the title you're receiving in that format. Either extract the source name from the title or from the url string. Add a max of 3 relevant hashtags at the end and make sure you stay in an acceptable length typical of a tweet";
 
 module.exports = async (titles) => {
   try {
